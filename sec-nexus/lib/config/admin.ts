c7bb25e9aa0cf564c7b472fd831df187b5ac488f
itsSambuddha <samskmailbox@gmail.com>
2025-11-04 21:39:09 +0530

    admin configured

--- sec-nexus/lib/config/admin.ts
+++ sec-nexus/lib/config/admin.ts
@@ -1,11 +1,23 @@
 export const HARD_CODED_ADMIN = {
-  clerkId: "user_32HzDnMpRW0GnWhPkgtHHA5bxb1",
+  clerkId: "user_351KkfQuhBUJ1VnNeeDxmiNlMYg",
   email: "shwetwondergeeks@gmail.com",
-  firstName: "Lets",
-  lastName: "SAM",
-  username: "letssay",
-  photo: "https://img.clerk.com/…"
+  firstName: "ShwetVeer",
+  lastName: "Vrish",
+  username: "principal",
+  photo: "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18zNTFLa2YxNGxrQ2FQOFFDT0c4azM2NHB0QXQifQ",
 } as const;
 
-
+// {
+//   "_id": {
+//     "$oid": "690a241d20f951a7fe9418a2"
+//   },
+//   "clerkId": "user_351KkfQuhBUJ1VnNeeDxmiNlMYg",
+//   "__v": 0,
+//   "email": "shwetwondergeeks@gmail.com",
+//   "firstName": "ShwetVeer",
+//   "lastName": "Vrish",
+//   "photo": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18zNTFLa2YxNGxrQ2FQOFFDT0c4azM2NHB0QXQifQ",
+//   "role": "student",
+//   "username": "principal"
+// }
 //register an user and take this info from the DB
